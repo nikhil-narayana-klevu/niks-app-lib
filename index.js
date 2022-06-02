ff4d11161d702ca60b7b1da163628baef60bfa8a
nikhil-narayana-klevu <97588976+nikhil-narayana-klevu@users.noreply.github.com>
2022-06-02 16:09:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export const name = 'Nikhil';
 export const anotherName = 'Karthikeya';
 export const anotherAnotherName = 'Shubha';
-export const someAnotherAnotherName = 'Abcd';
+export const someAnotherAnotherName = 'some another name';
